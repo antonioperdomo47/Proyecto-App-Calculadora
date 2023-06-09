a6d4f7ac962e1595171c43eb57fc18cd787a46d4
Antonio Perdomo <antonioperdomo30@gmail.com>
2023-06-09 19:19:46 -0400

    EuroLibraYen

--- calculadorawebscraping.py
+++ calculadorawebscraping.py
@@ -2,46 +2,65 @@ import requests
 from bs4 import BeautifulSoup
 import tkinter as tk
 
-def obtener_precio_euro_dolar():
-    url = 'https://es.investing.com/currencies/eur-usd'
-
-    # Realizar la solicitud GET al sitio web
+def obtener_precio_moneda(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        # Analizar el contenido HTML de la página
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Encontrar el elemento que contiene el valor del Euro-Dólar
         valor_element = soup.find('span', class_='text-2xl', attrs={'data-test': 'instrument-price-last'})
 
         if valor_element:
             valor = valor_element.text.strip()
+            valor = valor.replace(',', '.')  # Reemplazar la coma por un punto decimal
             return valor
         else:
-            return "No se encontró el valor del Euro-Dólar en la página."
+            return "No se encontró el valor de la moneda en la página."
     else:
         return f"No se pudo acceder a la página {url}."
 
 
 def actualizar_valor():
-    valor = obtener_precio_euro_dolar()
-    label_valor.config(text=valor)
+    url_euro_dolar = 'https://es.investing.com/currencies/eur-usd'
+    url_gbp_dolar = 'https://es.investing.com/currencies/gbp-usd'
+    url_jpy_dolar = 'https://es.investing.com/currencies/usd-jpy'
+    valor_euro_dolar = obtener_precio_moneda(url_euro_dolar)
+    valor_gbp_dolar = obtener_precio_moneda(url_gbp_dolar)
+    valor_jpy_dolar = obtener_precio_moneda(url_jpy_dolar)
+
+    valor_formateado_euro_dolar = f"Dollar {float(valor_euro_dolar):.2f}"
+    valor_formateado_gbp_dolar = f"Dollar {float(valor_gbp_dolar):.2f}"
+    valor_formateado_jpy_dolar = f"Dollar {float(valor_jpy_dolar):.2f}"
+
+    label_valor_euro.config(text=valor_formateado_euro_dolar)
+    label_valor_gbp.config(text=valor_formateado_gbp_dolar)
+    label_valor_jpy.config(text=valor_formateado_jpy_dolar)
 
 # Crear ventana
 ventana = tk.Tk()
-ventana.title("Valor del Euro-Dólar")
+ventana.title("Valores de Monedas")
 ventana.geometry("300x300")
 
-# Crear etiqueta para mostrar el valor
-label_valor = tk.Label(ventana, text="", font=("Arial", 24))
-label_valor.pack(pady=50)
+# Crear etiquetas para mostrar los valores
+label_euro = tk.Label(ventana, text="EUR-USD", font=("Arial", 14))
+label_euro.pack()
+label_valor_euro = tk.Label(ventana, text="", font=("Arial", 24))
+label_valor_euro.pack(pady=10)
 
-# Botón para actualizar el valor
+label_gbp = tk.Label(ventana, text="GBP-USD", font=("Arial", 14))
+label_gbp.pack()
+label_valor_gbp = tk.Label(ventana, text="", font=("Arial", 24))
+label_valor_gbp.pack(pady=10)
+
+label_jpy = tk.Label(ventana, text="JPY-USD", font=("Arial", 14))
+label_jpy.pack()
+label_valor_jpy = tk.Label(ventana, text="", font=("Arial", 24))
+label_valor_jpy.pack(pady=10)
+
+# Botón para actualizar los valores
 btn_actualizar = tk.Button(ventana, text="Actualizar", command=actualizar_valor)
 btn_actualizar.pack()
 
-# Actualizar el valor inicialmente
+# Actualizar los valores inicialmente
 actualizar_valor()
 
 # Iniciar el bucle principal de la ventana
